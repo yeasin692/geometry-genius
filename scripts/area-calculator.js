cd29e134694a0493ecd0a4aa9b84dbfc4ae20afe
yeasin692 <mdyeasin94987@gmail.com>
2023-08-19 12:53:41 +0600

    parallelogram

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -40,3 +40,30 @@ function calculateRectangleArea(){
     rectangleAreaSpan.innerText = area;
 
 }
+
+// reusable function --> reduce duplicate code
+function calculateParallelogramArea(){
+   const base = getInputValue('Parallelogram-base');
+//    console.log(base);
+
+   const height = getInputValue('Parallelogram-height');
+//    console.log(height);
+
+   const area = base * height;
+   setElementInnerText('Parallelogram-area', area)
+}
+
+// reusable get value field in number
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId );
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+
+}
+
+// reusable set span, p , div, etc, text
+function setElementInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
